247cfe355dd2f7f4639305fec4644d26af8adcfd
GvGMediaSolutions <66380959+GvGMediaSolutions@users.noreply.github.com>
2021-02-07 01:18:55 -0500

    Add files via upload

--- config.inc.php
+++ config.inc.php
@@ -3,5 +3,5 @@
 define("HOST", "localhost");
 define("USER", "root");
 define("PASS", "password");
-define("DB", "v-0.1alpha");
-define("SERVER", "test");
+define("SERVER", "v-0.1alpha");
+define("GAME", "test");
