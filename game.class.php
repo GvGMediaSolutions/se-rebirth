90a68af3c00c22e65ecf8b68f00ee1a77062d551
GvGMediaSolutions <66380959+GvGMediaSolutions@users.noreply.github.com>
2021-02-07 17:51:21 -0500

    Add files via upload

--- game.class.php
+++ game.class.php
@@ -2,51 +2,78 @@
 include_once 'mvc.class.php';
 
 class Game extends Db {
+  protected $table = "";
+  protected $fields = [];
+  protected $values = [];
+
   public function __construct($db = ""){
     //$this->create_db(SERVER);
-
     $this->db = !empty($db) ? $db : GAME;
-    //$this->create_table("daily_tips", " (`tip_id` int(4) NOT NULL auto_increment, `tip_content` text NOT NULL, PRIMARY KEY  (`tip_id`)) ENGINE=MyISAM");
-    //$this->create_table("(str)$on, "(str)$what");
+  }
 
-    // ~/~/~/ FORMATTING ABSTRACTED!
+  public function destroy(){
+    $this->drop_db(GAME);
+    $this->db = "";
+  }
 
-    //$this->update("daily_tips", array("tip_id", "tip_content"), array("tip_id"), array(2, 'You can change your colour scheme at any time from the options page.<br>There are plenty to choose from.', 1));
-    //$this->update("(str)$on", "(array)$what", "(array)$where", (array)$params);
+  public function makeNew(){
+    $this->db = "";
+    $this->drop_db(GAME);
+    $this->create_db(GAME);
+    $this->db = GAME;
+    $this->makeTableAccounts();
+  }
 
-    //$this->drop_db(SERVER);
-    //$this->drop_db("(str)$on");
+  // ~/~/~/ READ ME: Set $this->table to Game Database Name && Set $str to Game Database Creation SQL
+  public function makeTableAccounts(){
+    $this->table = "";
+    $str = "";
+    $this->drop_table($this->table);
+    $this->create_table($this->table, $str);
+    // ~/~/~/
+    //$this->makeUser('Owner', 'password', 1);
+    //$this->makeUser('Admin', 'password', 2);
+    //$this->makeUser('Test', 'password');
+  }
 
-    //$this->drop_table("daily_tips");
-    //$this->drop_table("(str)$on");
-
-    //$this->delete("daily_tips", "tip_id", array(2));
-    //$this->delete("(str)$on", "(str)$where", (array)$params);
-
-    //$this->select("user_accounts", "*", array("id", "name"), array(1, "Owner"));
-    //$this->select("(str)$on", "(str)$what", "(array)$where", (array)$params);
-
-    // ~/~/~/ FORMATTING ABSTRACTED!
-    //$this->insert("user_accounts", array("id", "name"), array(3, "Test"));
-    //$this->insert("(str)$on", "(array)$what", (array)$params);
-
-    public function destroy(){
-      $this->drop_db(GAME);
-      $this->db = "";
-    }
-
-    public function makeNew(){
-      $this->db = "";
-      $this->create_db(GAME);
-      $this->db = GAME;
-      $this->create_table("user_accounts", " ( `id` INT(4) NOT NULL AUTO_INCREMENT , `name` VARCHAR(32) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB");
-      $this->makeUser('Owner');
-    }
-
-    public function makeUser($name){
-      $this->insert("user_accounts", "(`name`)", "(?)", array($name));
-    }
+  // ~/~/~/ READ ME: Set $this->table to Game Database Name $$ Set $this->fields to array of field names && Set $this->values to array of values
+  public function makeUser($login_name, $password, $status = 0){
+    $this->table = "";
+    $this->fields = array();
+    $this->values = array();
+    $this->insert($this->table, $this->fields, $this->values);
+  }
 }
+
+$a = new Game();
+// ~/~/~/ READ ME: Drops ALL game databses and Drops root DATABASE
+$a->destroy();
+//Creates a brand new root database and creates the necessary tables
+$a->makeNew();
+
+//$this->create_table("daily_tips", " (`tip_id` int(4) NOT NULL auto_increment, `tip_content` text NOT NULL, PRIMARY KEY  (`tip_id`)) ENGINE=MyISAM");
+//$this->create_table("(str)$on, "(str)$what");
+
+// ~/~/~/ FORMATTING ABSTRACTED!
+
+//$this->update("daily_tips", array("tip_id", "tip_content"), array("tip_id"), array(2, 'You can change your colour scheme at any time from the options page.<br>There are plenty to choose from.', 1));
+//$this->update("(str)$on", "(array)$what", "(array)$where", (array)$params);
+
+//$this->drop_db(SERVER);
+//$this->drop_db("(str)$on");
+
+//$this->drop_table("daily_tips");
+//$this->drop_table("(str)$on");
+
+//$this->delete("daily_tips", "tip_id", array(2));
+//$this->delete("(str)$on", "(str)$where", (array)$params);
+
+//$this->select("user_accounts", "*", array("id", "name"), array(1, "Owner"));
+//$this->select("(str)$on", "(str)$what", "(array)$where", (array)$params);
+
+//$this->insert("user_accounts", array("id", "name"), array(3, "Test"));
+//$this->insert("(str)$on", "(array)$what", (array)$params);
+
 /* ~/~/~ TO DO
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -56,13 +83,23 @@ SET sql_mode =
 SET NAMES utf8mb4;
 */
 
-$a = new Game();
-$a->drop_table("accounts");
-$a->create_table("accounts", "(`id` int(11) NOT NULL AUTO_INCREMENT, `login_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL, `password` varchar(32) NOT NULL, `date_created` varchar(64) NOT NULL, `times_logged_in` int(10), `last_login` varchar(64), `ip_address` varchar(64), `banned` int(1), `status` varchar(32), `session_id` varchar(32), `games_joined` int(6), `msg_signature` varchar(255), `tokens` int(11), `player_image_path` varchar(255), `total_forum_posts` int(11), `forum_likes` int(11), `forum_dislikes` int(11), PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;");
-
 /* ~/~/~ TEST AREA
 
-$game = new Game();
-$game->destroy();
-$game->makeNew();
+//$env = new Game();
+//$env->destroy();
+//$env->makeNew();
+/*
+foreach($env->data[0] as $key => $val){
+  echo $key . ": " . $val . "<br>";
+}
+echo "<br>";
+*/
+
+/*
+for($i=0; $i<count($env->data); $i++){
+  foreach($env->data[$i] as $key => $val){
+    echo $key . ": " . $val . "<br>";
+  }
+  echo "<br>";
+}
 */
--- server.class.php
+++ server.class.php
@@ -2,31 +2,13 @@
 include_once 'mvc.class.php';
 
 class Server extends Db {
+  protected $table = "";
+  protected $fields = [];
+  protected $values = [];
+
   public function __construct(){
     //$this->create_db(SERVER);
     $this->db = SERVER;
-    //$this->create_table("daily_tips", " (`tip_id` int(4) NOT NULL auto_increment, `tip_content` text NOT NULL, PRIMARY KEY  (`tip_id`)) ENGINE=MyISAM");
-    //$this->create_table("(str)$on, "(str)$what");
-
-    // ~/~/~/ FORMATTING ABSTRACTED!
-    
-    //$this->update("daily_tips", array("tip_id", "tip_content"), array("tip_id"), array(2, 'You can change your colour scheme at any time from the options page.<br>There are plenty to choose from.', 1));
-    //$this->update("(str)$on", "(array)$what", "(array)$where", (array)$params);
-
-    //$this->drop_db(SERVER);
-    //$this->drop_db("(str)$on");
-
-    //$this->drop_table("daily_tips");
-    //$this->drop_table("(str)$on");
-
-    //$this->delete("daily_tips", "tip_id", array(2));
-    //$this->delete("(str)$on", "(str)$where", (array)$params);
-
-    //$this->select("user_accounts", "*", array("id", "name"), array(1, "Owner"));
-    //$this->select("(str)$on", "(str)$what", "(array)$where", (array)$params);
-
-    //$this->insert("user_accounts", array("id", "name"), array(3, "Test"));
-    //$this->insert("(str)$on", "(array)$what", (array)$params);
   }
 
   public function destroy(){
@@ -37,17 +19,60 @@ class Server extends Db {
 
   public function makeNew(){
     $this->db = "";
+    $this->drop_db(SERVER);
     $this->create_db(SERVER);
     $this->db = SERVER;
-    $this->create_table("user_accounts", " ( `id` INT(4) NOT NULL AUTO_INCREMENT , `name` VARCHAR(32) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB");
-    $this->makeUser('Owner');
+    $this->makeTableAccounts();
   }
 
-  public function makeUser($name){
-    $this->insert("user_accounts", "(`name`)", "(?)", array($name));
+  public function makeTableAccounts(){
+    $this->table = "accounts";
+    $str = "(`id` int(11) NOT NULL AUTO_INCREMENT, `login_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL, `password` varchar(32) NOT NULL, `date_created` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `times_logged_in` int(10), `last_login` timestamp(6), `ip_address` varchar(64), `banned` int(1), `status` varchar(32), `session_id` varchar(32), `games_joined` int(6), `msg_signature` varchar(255), `tokens` int(11), `player_image_path` varchar(255), `total_forum_posts` int(11), `forum_likes` int(11), `forum_dislikes` int(11), PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
+    $this->drop_table($this->table);
+    $this->create_table($this->table, $str);
+    $this->makeUser('Owner', 'password', 1);
+    $this->makeUser('Admin', 'password', 2);
+    $this->makeUser('Test', 'password');
+  }
+
+  public function makeUser($login_name, $password, $status = 0){
+    $this->table = "accounts";
+    $this->fields = array("login_name", "password", "status");
+    $this->values = array($login_name, $password, $status);
+    $this->insert($this->table, $this->fields, $this->values);
   }
 
 }
+
+$a = new Server();
+// ~/~/~/ READE ME: Drops ALL game databses and Drops root DATABASE
+$a->destroy();
+//Creates a brand new root database and creates the necessary tables
+$a->makeNew();
+
+//$this->create_table("daily_tips", " (`tip_id` int(4) NOT NULL auto_increment, `tip_content` text NOT NULL, PRIMARY KEY  (`tip_id`)) ENGINE=MyISAM");
+//$this->create_table("(str)$on, "(str)$what");
+
+// ~/~/~/ FORMATTING ABSTRACTED!
+
+//$this->update("daily_tips", array("tip_id", "tip_content"), array("tip_id"), array(2, 'You can change your colour scheme at any time from the options page.<br>There are plenty to choose from.', 1));
+//$this->update("(str)$on", "(array)$what", "(array)$where", (array)$params);
+
+//$this->drop_db(SERVER);
+//$this->drop_db("(str)$on");
+
+//$this->drop_table("daily_tips");
+//$this->drop_table("(str)$on");
+
+//$this->delete("daily_tips", "tip_id", array(2));
+//$this->delete("(str)$on", "(str)$where", (array)$params);
+
+//$this->select("user_accounts", "*", array("id", "name"), array(1, "Owner"));
+//$this->select("(str)$on", "(str)$what", "(array)$where", (array)$params);
+
+//$this->insert("user_accounts", array("id", "name"), array(3, "Test"));
+//$this->insert("(str)$on", "(array)$what", (array)$params);
+
 /* ~/~/~ TO DO
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -56,11 +81,6 @@ SET sql_mode =
 'NO_AUTO_VALUE_ON_ZERO';
 SET NAMES utf8mb4;
 */
-
-$a = new Server();
-//$a->drop_table("accounts");
-//$a->create_table("accounts", "(`id` int(11) NOT NULL AUTO_INCREMENT, `login_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL, `password` varchar(32) NOT NULL, `date_created` varchar(64) NOT NULL, `times_logged_in` int(10), `last_login` varchar(64), `ip_address` varchar(64), `banned` int(1), `status` varchar(32), `session_id` varchar(32), `games_joined` int(6), `msg_signature` varchar(255), `tokens` int(11), `player_image_path` varchar(255), `total_forum_posts` int(11), `forum_likes` int(11), `forum_dislikes` int(11), PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;");
-
 
 /* ~/~/~ TEST AREA
 
