d6adc092e2b0b7e7b9e3a785bf77e5420be1cb8c
GvGMediaSolutions <66380959+GvGMediaSolutions@users.noreply.github.com>
2021-02-08 13:47:34 -0500

    Add files via upload

--- classes/register.php
+++ classes/register.php
@@ -1,14 +1,24 @@
 <?php
   include_once 'server.class.php';
 
-  if((isset($_GET['name']) && isset($_GET['pass']) && isset($_GET['pass2']))){
-    $login_name = $_GET['name'];
-    $password = $_GET['pass'];
-    $password2 = $_GET['pass2'];
+  $out = "";
 
-    $server = new Server();
+  if(isset($_POST['submit'])){
+    if((isset($_POST['name']) && isset($_POST['pass']) && isset($_POST['pass2']))){
+      $login_name = $_POST['name'];
+      $password = $_POST['pass'];
+      $password2 = $_POST['pass2'];
 
-    $server->registerUser($login_name, $password, $password2);
+      $server = new Server();
+
+      $server->registerUser($login_name, $password, $password2);
+    }
+  }else{
+    $out = "<form action='register.php' method='post'>";
+    $out .= "<input type='text' name='name' placeholder='Username' autofocus><br>";
+    $out .= "<input type='password' name='pass' placeholder='Password'><br>";
+    $out .= "<input type='password' name='pass2' placeholder='Password Again'><br>";
+    $out .= "<input type='submit' name='submit'></form>";
   }
 
-  //echo $out;
+  echo $out;
