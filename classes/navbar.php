02ea3026c93b97de5278979b238c4dde6d764677
blindsight48 <78524879+blindsight48@users.noreply.github.com>
2021-02-09 06:55:01 -0500

    Create navbar.php

---
+++ classes/navbar.php
@@ -0,0 +1,25 @@
+<table style='margin:auto'>
+      <tr>
+        <td style='border:0px'>
+          <a href='gamelist.php'><h5 style='border-radius:25px;background-color:lightgrey;color:white;text-shadow:2px 2px black;box-shadow: 2px 2px black'>Account Info</h5></a>
+        </td>
+        <td style='border:0px'>
+          <a href='gamelist_msg.php'><h5 style='border-radius:25px;background-color:lightgrey;color:white;text-shadow:2px 2px black;box-shadow: 2px 2px black'>Messages</h5></a>
+        </td>
+        <td style='border:0px'>
+          <a href='gamelist_forum.php'><h5 style='border-radius:25px;background-color:lightgrey;color:white;text-shadow:2px 2px black;box-shadow: 2px 2px black'>Forum</h5></a>
+        </td>
+        <td style='border:0px'>
+          <a href='gamelist_games.php'><h5 style='border-radius:25px;background-color:lightgrey;color:white;text-shadow:2px 2px black;box-shadow: 2px 2px black'>Games</h5></a>
+        </td>
+        <td style='border:0px'>
+          <a href='gamelist_hof.php'><h5 style='border-radius:25px;background-color:lightgrey;color:white;text-shadow:2px 2px black;box-shadow: 2px 2px black'>Hall Of Fame</h5></a>
+        </td>
+        <td style='border:0px'>
+          <a href='help.php'><h5 style='border-radius:25px;background-color:lightgrey;color:white;text-shadow:2px 2px black;box-shadow: 2px 2px black'>Helpfiles</h5></a>
+        </td>
+        <td style='border:0px'>
+          <a href='logout.php'><h5 style='border-radius:25px;background-color:lightgrey;color:white;text-shadow:2px 2px black;box-shadow: 2px 2px black'>Logout</h5></a>
+        </td>
+      </tr>
+    </table>
